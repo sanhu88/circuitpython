578316772fb17e007cf3fb988d4b24e675e25901
Burt <504779282@qq.com>
2024-11-28 23:19:35 +0800

    BUZZER

--- ports/raspberrypi/boards/eetree_rp2040_game_kit/pins.c
+++ ports/raspberrypi/boards/eetree_rp2040_game_kit/pins.c
@@ -61,5 +61,7 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_LCD_D_C), MP_ROM_PTR(&pin_GPIO1) },
     { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display) },
 
+    { MP_ROM_QSTR(MP_QSTR_BUZZER), MP_ROM_PTR(&pin_GPIO23) },
+
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
